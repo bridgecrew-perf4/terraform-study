528da301720e5f7efe87e6e2462413a2189ed9aa
Kenichi Kato <k3nichikato@gmail.com>
2021-02-05 21:18:48 +0900

    VPCの作成とEC2の起動まで

--- aws/main.tf
+++ aws/main.tf
@@ -1,3 +1,6 @@
+############################
+#    Terraform provider
+############################
 terraform {
   required_providers {
     aws = {
@@ -12,7 +15,89 @@ provider "aws" {
   region  = "ap-northeast-1"
 }
 
-resource "aws_vpc" "vpc-aws1" {
-  cidr_block       = "192.168.37.0/24"
+############################
+#    VPC
+############################
+resource "aws_vpc" "vpc_kattest" {
+  cidr_block       = "172.16.0.0/16"
   instance_tenancy = "default"
+}
+
+resource "aws_internet_gateway" "gw_vpc_kattest" {
+  vpc_id = aws_vpc.vpc_kattest.id
+}
+
+resource "aws_route_table" "route_kattest" {
+  vpc_id = aws_vpc.vpc_kattest.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw_vpc_kattest.id
+  }
+}
+
+############################
+#    Management Subnet
+############################
+# Subnet
+resource "aws_subnet" "subnet_mgmt" {
+  vpc_id            = aws_vpc.vpc_kattest.id
+  availability_zone = "ap-northeast-1d"
+  cidr_block        = "172.16.213.0/24"
+
+  depends_on        = [aws_internet_gateway.gw_vpc_kattest]
+}
+
+resource "aws_route_table_association" "subnet_mgmt_route_kattest" {
+  subnet_id      = aws_subnet.subnet_mgmt.id
+  route_table_id = aws_route_table.route_kattest.id
+}
+
+# SG
+resource "aws_security_group" "sg_mgmt" {
+  name        = "sg_mgmt"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.vpc_kattest.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+############################
+#    EC2
+############################
+# key pair
+resource "aws_key_pair" "key_kattest" {
+  key_name   = "kattest"
+  public_key = file("/Users/kenichi/.ssh/id_rsa.pub")
+}
+# EC2
+resource "aws_instance" "sandbox_lin01" {
+  # ap-northeast-1
+  ami             = "ami-0992fc94ca0f1415a"
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.key_kattest.id
+  private_ip      = "172.16.213.11"
+  subnet_id       = aws_subnet.subnet_mgmt.id
+  security_groups = [aws_security_group.sg_mgmt.id]
+}
+
+# EIP
+resource "aws_eip" "eip_sandbox_lin01" {
+  vpc = true
+
+  instance                  = aws_instance.sandbox_lin01.id
+  associate_with_private_ip = aws_instance.sandbox_lin01.private_ip
+  depends_on                = [aws_internet_gateway.gw_vpc_kattest]
 }
